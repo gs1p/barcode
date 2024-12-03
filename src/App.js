41be957d2be36321cde6a799b4285bdfe0a3f9ca
gs1p <sadimanajaha@gmail.com>
2024-12-03 06:17:41 +0000

    Your commit message

--- src/App.js
+++ src/App.js
@@ -40,104 +40,104 @@ function App() {
         recommendation = `General Category
 
 
-Recommendation: Purchase 1 GTIN-13
-Entrance Fee: 22,695 (incl. Govt. Taxes)
-Annual Fee: 7,566 (incl. Govt. Taxes)
-Total Due: 30,261`;
+Recommendation: License 1 GTIN-13
+Entrance Fee: Rs 22,695 (incl. Govt. Taxes)
+Annual Fee: Rs 7,566 (incl. Govt. Taxes)
+Total Amount: Rs 30,261`;
       } else if (productNumber === 10) {
         recommendation = `General Category
 
 
-Recommendation: Purchase 10 GTIN-13s
-Entrance Fee: 22,695 (incl. Govt. Taxes)
-Annual Fee: 7,566 (incl. Govt. Taxes)
-Total Due: 30,261`;
+Recommendation: License 10 GTIN-13s
+Entrance Fee: Rs 22,695 (incl. Govt. Taxes)
+Annual Fee: Rs 7,566 (incl. Govt. Taxes)
+Total Amount: Rs 30,261`;
       } else if (productNumber <= 100) {
         recommendation = `General Category
 
 
-Recommendation: Purchase 100 GTIN-13s
-Entrance Fee: 45,392 (incl. Govt. Taxes)
-Annual Fee: 12,105 (incl. Govt. Taxes)
-Total Due: 57,497`;
+Recommendation: License 100 GTIN-13s
+Entrance Fee: Rs 45,392 (incl. Govt. Taxes)
+Annual Fee: Rs 12,105 (incl. Govt. Taxes)
+Total Amount: Rs 57,497`;
       } else if (productNumber <= 1000) {
         recommendation = `General Category
 
 
-Recommendation: Purchase 1,000 GTIN-13s
-Entrance Fee: 45,392 (incl. Govt. Taxes)
-Annual Fee: 30,261 (incl. Govt. Taxes)
-Total Due: 75,653`;
+Recommendation: License 1,000 GTIN-13s
+Entrance Fee: Rs 45,392 (incl. Govt. Taxes)
+Annual Fee: Rs 30,261 (incl. Govt. Taxes)
+Total Amount: Rs 75,653`;
       } else if (productNumber <= 10000) {
         recommendation = `General Category
 
 
-Recommendation: Purchase 10,000 GTIN-13s
-Entrance Fee: 45,392 (incl. Govt. Taxes)
-Annual Fee: 180,959 (incl. Govt. Taxes)
-Total Due: 226,351`;
+Recommendation: License 10,000 GTIN-13s
+Entrance Fee: Rs 45,392 (incl. Govt. Taxes)
+Annual Fee: Rs 180,959 (incl. Govt. Taxes)
+Total Amount: Rs 226,351`;
       }
     } else if (category === "textile") {
       if (productNumber <= 100) {
         recommendation = `Textile Category
 
 
-Recommendation: Purchase 100 GTIN-13s
-Entrance Fee: 45,392 (incl. Govt. Taxes)
-Annual Fee: 12,105 (incl. Govt. Taxes)
-Total Due: 57,497`;
+Recommendation: License 100 GTIN-13s
+Entrance Fee: Rs 45,392 (incl. Govt. Taxes)
+Annual Fee: Rs 12,105 (incl. Govt. Taxes)
+Total Amount: Rs 57,497`;
       } else if (productNumber <= 1000) {
         recommendation = `Textile Category
 
 
-Recommendation: Purchase 1,000 GTIN-13s
-Entrance Fee: 45,392 (incl. Govt. Taxes)
-Annual Fee: 30,261 (incl. Govt. Taxes)
-Total Due: 75,653`;
+Recommendation: License 1,000 GTIN-13s
+Entrance Fee: Rs 45,392 (incl. Govt. Taxes)
+Annual Fee: Rs 30,261 (incl. Govt. Taxes)
+Total Amount: Rs 75,653`;
       } else if (productNumber <= 10000) {
         recommendation = `Textile Category
 
 
-Recommendation: Purchase 10,000 GTIN-13s
-Entrance Fee: 45,392 (incl. Govt. Taxes)
-Annual Fee: 180,959 (incl. Govt. Taxes)
-Total Due: 226,351`;
+Recommendation: License 10,000 GTIN-13s
+Entrance Fee:  Rs 45,392 (incl. Govt. Taxes)
+Annual Fee: Rs 180,959 (incl. Govt. Taxes)
+Total Amount: Rs 226,351`;
       }
     } else if (category === "health care") {
       if (productNumber <= 100) {
         recommendation = `Healthcare Category
 
 
-Recommendation: Purchase 100 GTIN-13s
-Entrance Fee: 68,087 (incl. Govt. Taxes)
-Annual Fee: 18,156 (incl. Govt. Taxes)
-Total Due: 86,243`;
+Recommendation: License 100 GTIN-13s
+Entrance Fee: Rs 68,087 (incl. Govt. Taxes)
+Annual Fee: Rs 18,156 (incl. Govt. Taxes)
+Total Amount: Rs 86,243`;
       } else if (productNumber <= 1000) {
         recommendation = `Healthcare Category
 
 
-Recommendation: Purchase 1,000 GTIN-13s
-Entrance Fee: 68,087 (incl. Govt. Taxes)
-Annual Fee: 45,392 (incl. Govt. Taxes)
-Total Due: 113,479`;
+Recommendation: License 1,000 GTIN-13s
+Entrance Fee: Rs 68,087 (incl. Govt. Taxes)
+Annual Fee: Rs 45,392 (incl. Govt. Taxes)
+Total Amount: Rs 113,479`;
       }
     } else if (category === "UDI") {
       if (productNumber <= 100) {
         recommendation = `UDI Category
 
 
-Recommendation: Purchase 100 GTIN-13s
-Entrance Fee: 68,087 (incl. Govt. Taxes)
-Annual Fee: 54,468 (incl. Govt. Taxes)
-Total Due: 122,555`;
+Recommendation: License 100 GTIN-13s
+Entrance Fee: Rs 68,087 (incl. Govt. Taxes)
+Annual Fee: Rs 54,468 (incl. Govt. Taxes)
+Total Amount: Rs 122,555`;
       } else if (productNumber <= 1000) {
         recommendation = `UDI Category
 
 
-Recommendation: Purchase 1,000 GTIN-13s
-Entrance Fee: 68,087 (incl. Govt. Taxes)
-Annual Fee: 136,175 (incl. Govt. Taxes)
-Total Due: 204,262`;
+Recommendation: License 1,000 GTIN-13s
+Entrance Fee: Rs 68,087 (incl. Govt. Taxes)
+Annual Fee: Rs 136,175 (incl. Govt. Taxes)
+Total Amount: Rs 204,262`;
       }
     }
 
@@ -147,7 +147,7 @@ Total Due: 204,262`;
   return (
     <div className="app-container">
       <h1>Barcode Estimator</h1>
-      <h2>Any Individual/Firm/Company applying for the GTIN-13/GLN or GTIN-8 barcode numbers will be required to pay following Entrance and Annual Fees along with the GS1 Pakistan Standard Application Form duly completed.</h2>
+      <h2>Any individual, firm, or company applying for GTIN/GLN-13, or GTIN-8 barcode numbers will be required to pay following Entrance and Annual Fees along with the GS1 Pakistan Standard Application Form duly completed.</h2>
      
         
       <div className="input-container">
@@ -183,21 +183,10 @@ Please Indicate Below the Number of Global Trade Item Numbers (GTIN's) You Requi
       <div id="terms-details" class="terms-details">
   <h3>Terms and Conditions</h3>
   <p>
-    <strong>1. Entrance Fee Payment</strong><br />
-    Members availing the 50% discount on the Entrance Fee are required to pay the remaining amount of 
-    <strong> Rs. 16,304</strong> plus <strong>16% PRA</strong> when applying for additional numbers.
+     Annual fees are due one calendar year from the allocation date.<br />
+     From the second year onward, companies are required to pay the annual renewal fee.<br />
   </p>
   <p>
-    <strong>2. Annual Fees</strong><br />
-    - Annual fees are due one calendar year from the allocation date.<br />
-    - From the second year onward, companies are required to pay the annual renewal fee.<br />
-    - For example:<br />
-      &nbsp;&nbsp;&nbsp;- If you request 300 GTIN-13s, you will need to pay <strong>Rs. 60,523/- </strong> 
-      (Entrance + Annual Fees) for the first year.<br />
-      &nbsp;&nbsp;&nbsp;- For subsequent years, the annual renewal fee will be <strong>Rs. 15,131/-</strong>.
-  </p>
-  <p>
-    <strong>3. Late Fee Charges</strong><br />
     A late fee charge of <strong>5%</strong> will be applied to the total invoice amount after the due date.
   </p>
 </div>
@@ -208,7 +197,16 @@ Please Indicate Below the Number of Global Trade Item Numbers (GTIN's) You Requi
         <div className="popup-container">
           <div className="popup-content">
             <h3>Please Contact Us</h3>
-            <p>The number of products entered exceeds the limit for your selected category. Please contact us for more details.</p>
+            <p>The number of products entered exceeds the limit for your selected category. Please contact us for more details.
+            <br />
+Call our member support team on freephone 021 32215844 or +92 (312) 0003997 or +92 (322) 2251704.
+
+<br />
+Email us at info@gs1pk.org and we'll aim to get back to you within 2-3 working days.
+<br />
+Address
+Office No.B-2, 2nd Floor, Azzainab Court, Campbell Street, Karachi-74200, Pakistan
+            </p>
             <button onClick={handleClosePopup}>Close</button>
           </div>
         </div>
